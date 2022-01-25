45190b34ad3cc0e1e6f367c7e613005e330cc14a
lazo <lazo28.09.77@gmail.com>
2022-01-25 15:58:44 +0200

    header+img+common

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.scss';
-import {WelcomePage} from './components/welcomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
 
-      </header>
-      <WelcomePage/>
     </div>
   );
 }

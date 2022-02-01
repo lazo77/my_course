198ebfc8c8256b16b760e68962265b8e967c702d
lazo <lazo28.09.77@gmail.com>
2022-02-01 16:28:56 +0200

    start routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { MainPage } from './components/mainPage';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <header>
+        <Link to='/'>Главная</Link>
+        <Link to='/about'>О нас</Link>
+        <Link to='/courses'>Курсы</Link>
+      </header>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/courses' element={<Courses />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
 
